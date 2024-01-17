af32ca6c688cb4980f132063f0565664ea5f2ed9
Hitver Cruz G <hittcruz@gmail.com>
2024-01-17 18:38:33 -0500

    creando nueva app

---
+++ src/typescript/TiposBasicos.tsx
@@ -0,0 +1,10 @@
+export const TiposBasicos = () => {
+  let nombre: string | number = "Fernando";
+  nombre = 123;
+  return (
+    <>
+      <h3>Tipos Basicos</h3>
+      {nombre}
+    </>
+  );
+};
